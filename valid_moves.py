1e3b591b445b2ecc0ae6532c22087b02bbb1a4d9
joelrosen1 <rosen.jo@northeastern.edu>
2024-03-30 18:50:42 -0400

    Checked for moves out of bounds

--- valid_moves.py
+++ valid_moves.py
@@ -1,44 +1,51 @@
 #Checks moves with jumps
 def check_jumps(board, node, player1, player2, moves):
     row, col = node
-    
-     #If spot to the right is occupied and the spot after is empty
-    if (board[row][col+2] == player1 or board[row][col+2] == player2) and  (board[row][col+4] == 0):
+    length = len(board[0])
+    width = len(board[1])
+    #If spot to the right is occupied and the spot after is empty
+    if (col + 2 < width and col + 4 < width and 
+        board[row][col+2] == player1 or board[row][col+2] == player2) and  (board[row][col+4] == 0):
         jumpboard = board.copy()
         #This is so in the recursion there isn't an infinte loop of jumping back and forth
         jumpboard[row][col] = -1
         moves.append((row, col+4))
         check_jumps(jumpboard, (row, col+4), player1, player2, moves)
     #If spot to the left is occupied and the spot after is empty
-    if (board[row][col-2] == player1 or board[row][col-2] == player2) and  (board[row][col-4] == 0):
+    if (col - 2 < width and col - 4 < width and 
+        board[row][col-2] == player1 or board[row][col-2] == player2) and  (board[row][col-4] == 0):
         jumpboard = board.copy()
         #This is so in the recursion there isn't an infinte loop of jumping back and forth
         jumpboard[row][col] = -1
         moves.append((row, col-4))
         check_jumps(jumpboard, (row, col-4), player1, player2, moves)
     #If spot to the top right is occupied and the spot after is empty
-    if (board[row-1][col+1] == player1 or board[row-1][col+1] == player2) and  (board[row-2][col+2] == 0):
+    if (row - 1 < length and col + 1 < width and row - 2 < length and col + 2 < width and 
+        board[row-1][col+1] == player1 or board[row-1][col+1] == player2) and  (board[row-2][col+2] == 0):
         jumpboard = board.copy()
         #This is so in the recursion there isn't an infinte loop of jumping back and forth
         jumpboard[row][col] = -1
         moves.append((row-2, col+2))
         check_jumps(jumpboard, (row-2, col+2), player1, player2, moves)
     #If spot to the top left is occupied and the spot after is empty
-    if (board[row-1][col-1] == player1 or board[row-1][col-1] == player2) and  (board[row-2][col-2] == 0):
+    if (row - 1 < length and col - 1 < width and row - 2 < length and col - 2 < width and 
+        board[row-1][col-1] == player1 or board[row-1][col-1] == player2) and  (board[row-2][col-2] == 0):
         jumpboard = board.copy()
         #This is so in the recursion there isn't an infinte loop of jumping back and forth
         jumpboard[row][col] = -1
         moves.append((row-2, col-2))
         check_jumps(jumpboard, (row-2, col-2), player1, player2, moves)
     #If spot to the bottom left is occupied and the spot after is empty
-    if (board[row+1][col-1] == player1 or board[row+1][col-1] == player2) and  (board[row+2][col-2] == 0):
+    if (row + 1 < length and col - 1 < width and row + 2 < length and col - 2 < width and 
+        board[row+1][col-1] == player1 or board[row+1][col-1] == player2) and  (board[row+2][col-2] == 0):
         jumpboard = board.copy()
         #This is so in the recursion there isn't an infinte loop of jumping back and forth
         jumpboard[row][col] = -1
         moves.append((row+2, col-2))
         check_jumps(jumpboard, (row+2, col-2), player1, player2, moves)
     #If spot to the bottom right is occupied and the spot after is empty
-    if (board[row+1][col+1] == player1 or board[row+1][col+1] == player2) and  (board[row+2][col+2] == 0):
+    if (row + 1 < length and col + 1 < width and row + 2 < length and col + 2 < width and
+        board[row+1][col+1] == player1 or board[row+1][col+1] == player2) and  (board[row+2][col+2] == 0):
         jumpboard = board.copy()
         #This is so in the recursion there isn't an infinte loop of jumping back and forth
         jumpboard[row][col] = -1
@@ -57,65 +64,74 @@ def valid_moves(board, node, player1, player2):
     row = node[0]
     col = node[1]
     
+    length = len(board[0])
+    width = len(board[1])
+    
     #Checking if you can move one to the right
-    if board[row][col+2] == 0:
+    if col + 2 < width and board[row][col+2] == 0:
         moves.append((row, col+2))
     #Checking if you can move one to the left
-    if board[row][col-2] == 0:
+    if col - 2 < width and board[row][col-2] == 0:
         moves.append((row, col-2))
     #Checking if you can move up left
-    if board[row-1][col-1] == 0:
+    if row - 1 < length and col - 1 < width and board[row-1][col-1] == 0:
         moves.append((row-1, col-1))
     #Checking if you can move up right
-    if board[row-1][col+1] == 0:
+    if row - 1 < length and col + 1 < width and board[row-1][col+1] == 0:
         moves.append((row-1, col+1))
     #Checking if you can move down left
-    if board[row+1][col-1] == 0:
+    if row + 1 < length and col - 1 < width and board[row+1][col-1] == 0:
         moves.append((row+1, col-1))
     #Checking if you can move down right
-    if board[row+1][col+1] == 0:
+    if row + 1 < length and col + 1 < width and board[row+1][col+1] == 0:
         moves.append((row+1, col+1))
         
     #Checking for jumps
     #Adds each possible move on a link of jumps
     
     #If spot to the right is occupied and the spot after is empty
-    if (board[row][col+2] == player1 or board[row][col+2] == player2) and  (board[row][col+4] == 0):
+    if (col + 2 < width and col + 4 < width and 
+        board[row][col+2] == player1 or board[row][col+2] == player2) and  (board[row][col+4] == 0):
         jumpboard = board.copy()
         #This is so in the recursion there isn't an infinte loop of jumping back and forth
         jumpboard[row][col] = -1
         moves.append((row, col+4))
         check_jumps(jumpboard, (row, col+4), player1, player2, moves)
     #If spot to the left is occupied and the spot after is empty
-    if (board[row][col-2] == player1 or board[row][col-2] == player2) and  (board[row][col-4] == 0):
+    if (col - 2 < width and col - 4 < width and 
+        board[row][col-2] == player1 or board[row][col-2] == player2) and  (board[row][col-4] == 0):
         jumpboard = board.copy()
         #This is so in the recursion there isn't an infinte loop of jumping back and forth
         jumpboard[row][col] = -1
         moves.append((row, col-4))
         check_jumps(jumpboard, (row, col-4), player1, player2, moves)
     #If spot to the top right is occupied and the spot after is empty
-    if (board[row-1][col+1] == player1 or board[row-1][col+1] == player2) and  (board[row-2][col+2] == 0):
+    if (row - 1 < length and col + 1 < width and row - 2 < length and col + 2 < width and 
+        board[row-1][col+1] == player1 or board[row-1][col+1] == player2) and  (board[row-2][col+2] == 0):
         jumpboard = board.copy()
         #This is so in the recursion there isn't an infinte loop of jumping back and forth
         jumpboard[row][col] = -1
         moves.append((row-2, col+2))
         check_jumps(jumpboard, (row-2, col+2), player1, player2, moves)
     #If spot to the top left is occupied and the spot after is empty
-    if (board[row-1][col-1] == player1 or board[row-1][col-1] == player2) and  (board[row-2][col-2] == 0):
+    if (row - 1 < length and col - 1 < width and row - 2 < length and col - 2 < width and 
+        board[row-1][col-1] == player1 or board[row-1][col-1] == player2) and  (board[row-2][col-2] == 0):
         jumpboard = board.copy()
         #This is so in the recursion there isn't an infinte loop of jumping back and forth
         jumpboard[row][col] = -1
         moves.append((row-2, col-2))
         check_jumps(jumpboard, (row-2, col-2), player1, player2, moves)
     #If spot to the bottom left is occupied and the spot after is empty
-    if (board[row+1][col-1] == player1 or board[row+1][col-1] == player2) and  (board[row+2][col-2] == 0):
+    if (row + 1 < length and col - 1 < width and row + 2 < length and col - 2 < width and 
+        board[row+1][col-1] == player1 or board[row+1][col-1] == player2) and  (board[row+2][col-2] == 0):
         jumpboard = board.copy()
         #This is so in the recursion there isn't an infinte loop of jumping back and forth
         jumpboard[row][col] = -1
         moves.append((row+2, col-2))
         check_jumps(jumpboard, (row+2, col-2), player1, player2, moves)
     #If spot to the bottom right is occupied and the spot after is empty
-    if (board[row+1][col+1] == player1 or board[row+1][col+1] == player2) and  (board[row+2][col+2] == 0):
+    if (row + 1 < length and col + 1 < width and row + 2 < length and col + 2 < width and
+        board[row+1][col+1] == player1 or board[row+1][col+1] == player2) and  (board[row+2][col+2] == 0):
         jumpboard = board.copy()
         #This is so in the recursion there isn't an infinte loop of jumping back and forth
         jumpboard[row][col] = -1
