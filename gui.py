684a11e0cac311cf1cd837da34a05cdd3524e7ec
ryan howe <ryan.wm.howe@gmail.com>
2024-04-03 11:00:44 -0400

    ai makes moves but needs more work

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@ from valid_moves import *
 from game_initialization import *
 from game_over import *
 from move import *
+from minimax import *
 
 def distance(p1, p2):
     """Calculate the distance between two points."""
@@ -11,6 +12,10 @@ def distance(p1, p2):
     x2, y2 = p2
     return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
+def get_next_turn(current_turn):
+    if (current_turn == 6):
+        return 1
+    return current_turn+1
 
 def gen_gui(board):
     # Define colors
@@ -20,8 +25,9 @@ def gen_gui(board):
     YELLOW = (236, 252, 2)
     GREEN = (32, 252, 2)
     BLUE = (3, 65, 252)
-    PURPLE = (202, 3, 252)
+    PURPLE = (247, 118, 5)
     PINK = (255, 0, 247)
+    player_turn = 6
 
     # Define constants for the game board
     CELL_SIZE = 30
@@ -48,12 +54,30 @@ def gen_gui(board):
         5: PURPLE,
         6: BLUE
     }
-    current_player = 1
+    current_player = 6
     first_click = None
 
     # Main loop
     while True:
         # Handle events
+        if (current_player != 6):
+            #current_player = get_next_turn(current_player)
+            start_end_dict = {}
+            # do the best move for the ai
+            for i, row in enumerate(board):
+                for j, cell in enumerate(row):
+                    if (board[i][j] == current_player):
+                        all_moves = valid_moves(board, (i,j), current_player)
+                        best_move = get_best_move(board, all_moves, (i,j), current_player)
+                        if (best_move is not None):
+                            starting_tuple = (i,j)
+                            start_end_dict[starting_tuple] = best_move
+                            move(board, i, j, best_move[0], best_move[1])
+            # opt_move = best_of_best(board,start_end_dict,player_turn)
+            # move(board, opt_move[0][0], opt_move[0][1], opt_move[1][0], opt_move[1][1])
+            current_player = get_next_turn(current_player)
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -68,14 +92,14 @@ def gen_gui(board):
                             circle_center = (PADDING + j * CELL_SIZE + CELL_SIZE // 2, PADDING + i * CELL_SIZE + CELL_SIZE // 2)
                             if distance(mouse_pos, circle_center) <= MARBLE_RADIUS:
                                 m = board[i][j]
-                                if (m == current_player and first_click is None):
+                                if (m == 6 and first_click is None):
                                     first_click = (i,j)
                                 elif (first_click is not None and m == 0):
                                     second_click = (i,j)
-                                    if (valid_moves(board, first_click, current_player).__contains__(second_click)):
+                                    if (valid_moves(board, first_click, 6).__contains__(second_click)):
                                         print("movin off clicks")
                                         move(board, first_click[0], first_click[1], second_click[0], second_click[1])
-                                        current_player = current_player % 6 + 1
+                                        current_player = get_next_turn(current_player)
                                     else:
                                         print("Invalid move try again!")
                                     first_click = None
@@ -99,9 +123,7 @@ def gen_gui(board):
                 for j, cell in enumerate(row):
                     if (valid_moves(board, first_click, 6).__contains__((i,j))):  
                         color = (153,204,255)
-                        # Draw filled circle for marble
                         pygame.draw.circle(screen, color, (PADDING + j * CELL_SIZE + CELL_SIZE // 2, PADDING + i * CELL_SIZE + CELL_SIZE // 2), MARBLE_RADIUS)
-                        # Draw outline circle
                         pygame.draw.circle(screen, BLACK, (PADDING + j * CELL_SIZE + CELL_SIZE // 2, PADDING + i * CELL_SIZE + CELL_SIZE // 2), MARBLE_RADIUS, OUTLINE_WIDTH)
 
         # Update the display
---
+++ minimax.py
@@ -0,0 +1,53 @@
+from valid_moves import *
+from game_initialization import *
+from game_over import *
+from move import *
+import math
+
+def distance_to_goal_node(goal, current):
+    x1, y1 = goal
+    x2, y2 = current
+    return math.sqrt(((x2 - x1)/2) ** 2 + (y2 - y1) ** 2)
+
+def calculate_distance(board, player):
+    goal_node_list = [(16,12),(12,24),(12,0),(4,24),(4,0),(0,12)]
+    goal_node = goal_node_list[player-1]
+    total_distance = 0
+    for i, row in enumerate(board):
+            for j, cell in enumerate(row):
+                cell = (i,j)
+                if (cell == player):
+                    distance = distance_to_goal_node(goal_node, cell)
+                    print("distance for single node to goal node", distance)
+                    total_distance += distance
+
+    return total_distance
+
+def get_best_move(board, moves, current, player):
+    print("getting best move from", current, "player", player)
+    board_copy = board.copy()
+    potential_move_distances = []
+    for valid_move in moves:
+        print("hitting a valid move")
+        board = board_copy.copy()
+        move(board, current[0], current[1], valid_move[0], valid_move[1])
+        updated_distance = calculate_distance(board, player)
+        potential_move_distances.append(updated_distance)
+
+    if not potential_move_distances:
+        return None
+    print("list", potential_move_distances)
+    min_index = potential_move_distances.index(min(potential_move_distances))
+    best_move = moves[min_index]
+    # get the index of the min in potential omve distances
+    # return the corresponding move fro moves
+    # this function returns the best slot that the selected marble should go tom
+    return best_move
+
+def best_of_best(board, dictionary, playerturn):
+    distance_list = []
+    for key in dictionary:
+        val = dictionary[key]
+        board_copy = board.copy()
+    return
+
